6037f019de48db3ce0a3a1d970d3fb05edd92ac2
Fazal ul Rehman <fazalshahid58@gmail.com>
2024-08-26 20:01:33 +0500

    Update game.py
    Updated 26-aug-2024

--- game.py
+++ game.py
@@ -1,33 +1,33 @@
 import random
 
-def get_choices() :
-    player_choice = input("Enter a choice (rock, paper, scissors): ")
-    options = ["rock", "paper", "scissors"]
+def get_choices():
+    options = ['rock', 'paper', 'scissors']
+    user_choice = input("Enter your choice (Rock, Paper, Scissors): ")
     computer_choice = random.choice(options)
-    choices = {"player" : player_choice, "computer" : computer_choice}
+    
+    choices = {'user_choice' : user_choice, 'computer_choice': computer_choice}
     return choices
-
 
 def check_win(player, computer):
     if player == computer:
-     return "It's a tie!"
-    elif player == "rock":
-        if computer == "scissors":
-          return "Rock smashes scissors! You win!"
-        else: 
-             return "paper covers rock! You lose."
-    elif player == "paper":
-        if computer == "scissors":
-            return "scissors cut paper! You lose."
+        print(f"Computer choice: {computer} | It's a tie")
+    elif player == 'rock':
+        if computer == 'scissors':
+            print(f"Computer choice: {computer} | Rock break scissor. You Win")
         else:
-            return "paper covers rock. You win!"
-    elif player == "scissors":
-        if computer == "rock":
-            return "Rock smashes scissors! You lose."
+            print(f"Computer choice: {computer} |  Paper covers Rock. You Lose")
+    elif player == 'scissor':
+        if computer == 'rock':
+            print(f"Computer choice: {computer} | Rock beats scissors. You lost")
         else:
-            return "scissor cut paper. You win!"
-
+            print(f"Computer choice: {computer} | Scissor cuts paper. You win")
+    elif player == 'paper':
+        if computer == 'Rock':
+            print(f"Computer choice: {computer} | Paper covers rock. You win")
+        else:
+            print(f"Computer choice: {computer} | Scissor cuts paper. You Lost")
+    
+    
 choices = get_choices()
-result = check_win(choices["player"], choices["computer"])
-print(choices)
-print(result)
+result = check_win(choices['user_choice'], choices['computer_choice'])
+    
